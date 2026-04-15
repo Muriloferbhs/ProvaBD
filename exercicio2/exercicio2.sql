42dccceaf98d60a259e0217604d3c20c1e02b1ca
Fabiano3213 <josefabiano874@gmail.com>
2026-04-14 23:17:34 -0300

    Refactor SQL schema and insert statements

--- exercicio2/exercicio2.sql
+++ exercicio2/exercicio2.sql
@@ -1,48 +1,78 @@
-
 CREATE TABLE Curso (
-    IDCurso INT PRIMARY KEY,
-    nome NVARCHAR(100),
-    departamento NVARCHAR(100),
-    NumeroCurso INT 
+IDCurso INT PRIMARY KEY,
+nome NVARCHAR(100),
+departamento NVARCHAR(100),
+NumeroCurso INT
 );
 
 CREATE TABLE Sala (
-    IDSala INT PRIMARY KEY,
-    NumeroSala INT,
-    capacidade INT,
-    predio NVARCHAR(100)
+IDSala INT PRIMARY KEY,
+NumeroSala INT,
+capacidade INT,
+predio NVARCHAR(100)
 );
 
 CREATE TABLE Secao (
-    IDSecao INT PRIMARY KEY,
-    numero_secao INT,
-    programa NVARCHAR(100),
-    IDCurso INT,
-    FOREIGN KEY (IDCurso) REFERENCES Curso(IDCurso)
+IDSecao INT PRIMARY KEY,
+numero_secao INT,
+programa NVARCHAR(100),
+IDCurso INT,
+FOREIGN KEY (IDCurso) REFERENCES Curso(IDCurso)
 );
 
 CREATE TABLE Exame (
-    IDExame INT PRIMARY KEY,
-    horario DATETIME,
-    IDSecao INT,
-    IDSala INT,
-    FOREIGN KEY (IDSecao) REFERENCES Secao(IDSecao),
-    FOREIGN KEY (IDSala) REFERENCES Sala(IDSala)
+IDExame INT PRIMARY KEY,
+horario DATETIME,
+IDSecao INT,
+IDSala INT,
+FOREIGN KEY (IDSecao) REFERENCES Secao(IDSecao),
+FOREIGN KEY (IDSala) REFERENCES Sala(IDSala)
 );
 
-INSERT INTO Curso VALUES (1, 'Banco de Dados', 'TI', 101),
-(2, 'Algoritmos', 'TI', 102);
 
-INSERT INTO Sala VALUES (1, 101, 40, 'Bloco A'),
-(2, 202, 30, 'Bloco B');
+INSERT INTO Curso VALUES
+(1, 'Banco de Dados', 'TI', 101),
+(2, 'Algoritmos', 'TI', 102),
+(3, 'Engenharia de Software', 'TI', 103),
+(4, 'Redes de Computadores', 'TI', 104),
+(5, 'Inteligência Artificial', 'TI', 105),
+(6, 'Segurança da Informação', 'TI', 106),
+(7, 'Computação em Nuvem', 'TI', 107);
 
-INSERT INTO Secao VALUES (1, 1, 'Noturno', 1),
-(2, 2, 'Diurno', 2);
+INSERT INTO Sala VALUES
+(1, 101, 40, 'Bloco A'),
+(2, 202, 30, 'Bloco B'),
+(3, 303, 50, 'Bloco C'),
+(4, 404, 35, 'Bloco D'),
+(5, 505, 60, 'Bloco E'),
+(6, 606, 45, 'Bloco F');
 
-INSERT INTO Exame VALUES (1, '2026-06-10 19:00', 1, 1),
-(2, '2026-06-11 08:00', 2, 2);
 
-SELECT 
+INSERT INTO Secao VALUES
+(1, 1, 'Noturno', 1),
+(2, 2, 'Diurno', 2),
+(3, 1, 'Noturno', 3),
+(4, 2, 'Diurno', 3),
+(5, 1, 'Noturno', 4),
+(6, 2, 'Diurno', 5),
+(7, 1, 'Noturno', 6),
+(8, 2, 'Diurno', 7);
+
+
+INSERT INTO Exame VALUES
+(1, '2026-06-10 19:00', 1, 1),
+(2, '2026-06-11 08:00', 2, 2),
+(3, '2026-06-12 19:00', 3, 3),
+(4, '2026-06-13 08:00', 4, 4),
+(5, '2026-06-14 19:00', 5, 5),
+(6, '2026-06-15 08:00', 6, 3),
+(7, '2026-06-16 19:00', 7, 6),
+(8, '2026-06-17 08:00', 8, 2),
+(9, '2026-06-18 19:00', 1, 4),
+(10, '2026-06-19 08:00', 3, 5);
+
+
+SELECT
 	Exame.IDExame AS id_exame,
 	Exame.horario AS horario_exame,
 	Curso.nome AS nome_curso,
@@ -57,7 +87,7 @@ INNER JOIN Secao ON Exame.IDSecao = Secao.IDSecao
 INNER JOIN Curso ON Secao.IDCurso = Curso.IDCurso
 INNER JOIN Sala ON Exame.IDSala = Sala.IDSala;
 
-SELECT 
+SELECT
 	Curso.nome AS nome_curso,
 	Curso.NumeroCurso AS numero_curso,
 	Secao.numero_secao AS numero_secao,
@@ -69,3 +99,4 @@ FROM Secao
 LEFT JOIN Curso ON Secao.IDCurso = Curso.IDCurso
 LEFT JOIN Exame ON Exame.IDSecao = Secao.IDSecao
 LEFT JOIN Sala ON Exame.IDSala = Sala.IDSala;
+
